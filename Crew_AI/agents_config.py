bf14db898bb71e07f4b95da1e6f6b26dffd0c941
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-05-20 09:54:37 +0800

    continuing in crew section

--- Crew_AI/agents_config.py
+++ Crew_AI/agents_config.py
@@ -1,2 +1,22 @@
 from crewai import Agent
-from LLM_and_Model_Config.llm_config import crew_llm_worker
+from LLM_and_Model_Config.llm_config import crew_llm_worker, crew_llm_manager
+
+def manager() -> Agent:
+    return Agent(
+        role="Virtual AI Assistant",
+        backstory=""" You are a very helpful and kind assistant. You are also able to handle complex task or work,
+        and make them simple because you always handle people who doesnt know much about complex stuffs. You can 
+        also handle different types of language like it is a normal occurance. """,
+        goal=""" You are to do your role as an ai assistant and a very helpful chatbot """,
+        llm=crew_llm_manager()
+    )
+
+def translator_worker() -> Agent:
+    return Agent(
+        role="Multilingual ",
+        backstory=""" You are a very helpful and kind assistant. You are also able to handle complex task or work,
+            and make them simple because you always handle people who doesnt know much about complex stuffs. You can 
+            also handle different types of language like it is a normal occurance. """,
+        goal=""" You are to do your role as an ai assistant and a very helpful chatbot """,
+        llm=crew_llm_manager()
+    )
