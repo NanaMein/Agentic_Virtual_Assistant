8802e2e0483a75157d457af6351a7402d01cbd10
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-05-20 23:03:32 +0800

    moved directory

---
+++ Flow_Crew_AI/Memory_Layer/memory_for_past_context.py
@@ -0,0 +1,86 @@
+from mem0 import Memory
+from dotenv import load_dotenv
+from LLM_and_Model_Config.llm_config import llama_llm_small, llama_llm_big
+import os
+
+load_dotenv()
+
+url = os.getenv('URI')
+token = os.getenv('TOKEN')
+groq_llm = os.getenv('LLM_SMALL')
+groq = os.getenv('API_KEY')
+os.environ['GROQ_API_KEY'] = groq
+
+
+
+config = {
+    "vector_store": {
+        "provider": "milvus",
+        "config": {
+            "collection_name": "test_mem0_memory",
+            "url": url,
+            "token": token,
+            "embedding_model_dims": "768",  # Change this according to your local model's dimensions
+        },
+    },
+    "llm": {
+        "provider": "groq",
+        "config": {
+            "model": groq_llm,
+            "temperature": 0.2,
+            "max_tokens": 3000,
+        },
+    },
+    "embedder": {
+        "provider": "huggingface",
+        "config": {"model": "intfloat/e5-base"},
+    },
+    # "history_db_path": "/path/to/history.db",
+    "version": "v1.1"
+}
+
+m = Memory.from_config(config)
+
+
+def safe_text(text: str) -> str:
+    return text.replace('"', '\\"').replace("'", "\\'")
+
+def add_memories(user_input: str, ai_output: str) -> bool:
+    try:
+        input_text = f"{safe_text(user_input)}"
+        output_text = f"{safe_text(ai_output)}"
+
+        messages = [
+            {"role": "user", "content": input_text},
+            {"role": "assistant", "content": output_text},
+        ]
+        m.add(messages, user_id="Alfeo", metadata={"category": "default"})
+        return True
+    except Exception as e:
+        # Optional: log the exception details for debugging
+        print(f"Error adding memories: {e}")
+        return False
+
+
+def add_memories_v1(user_input: str, ai_output: str):
+    messages = [
+        {"role": "user", "content": user_input},
+        {"role": "assistant", "content": ai_output},
+    ]
+    m.add(messages, user_id="Alfeo", metadata={"category" : "default"})
+
+
+def search_memories(user_query: str):
+    related_memories = m.search(user_query, user_id="Alfeo")
+    return related_memories
+
+def chat_memories( user_chat: str):
+    chat_completion = m.chat(user_chat)
+    return chat_completion
+
+def previous_memories():
+    all_memo = m.get_all(user_id="Alfeo", limit=5)
+    return all_memo
+
+def reset_memories():
+    m.reset()
