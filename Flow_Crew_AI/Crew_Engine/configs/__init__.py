ba0c018dd8a8659ef2fb4cdb0081c061525ec88c
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-05-20 23:10:30 +0800

    rename folder of Crew

---
+++ Flow_Crew_AI/Crew_Engine/configs/__init__.py
@@ -0,0 +1,3 @@
+from Team_A_Crew.configs.agents_config import manager_agent, translator_agent
+from Team_A_Crew.configs.crew_config import crew
+from Team_A_Crew.configs.tasks_config import first_task
