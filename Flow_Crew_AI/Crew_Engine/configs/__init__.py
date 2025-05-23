1af46074ffdac8c30955249645f99e7e9e1cbe0f
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-05-24 03:14:06 +0800

    update chat completion section

--- Flow_Crew_AI/Crew_Engine/configs/__init__.py
+++
@@ -1,3 +0,0 @@
-from Team_A_Crew.configs.agents_config import manager_agent, translator_agent
-from Team_A_Crew.configs.crew_config import crew
-from Team_A_Crew.configs.tasks_config import first_task
